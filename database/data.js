841250e396362e379aa098c83fddb914fb2b064d
Prashanth <prashanthsai189011412@gmail.com>
2023-12-18 14:44:50 +0530

    final server side code

---
+++ database/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        id: 1,
+        question : "What is the correct spelling for the past tense of the verb 'go'?",
+        options : [
+            'goed',
+            'went',
+            'goeded',
+        ]
+    },
+    {
+        id: 2,
+        question : "Which sentence uses the correct form of the verb 'to be' in the present tense",
+        options : [
+            'She am happy',
+            'They is excited',
+            'He is hungry',
+        ]
+    },
+    {
+        id: 3,
+        question : "Choose the sentence that uses the correct comparative form",
+        options : [
+            'She runs more faster than him',
+            'This car is fastest than the other',
+            'He is taller than her',
+        ]
+    },
+    {
+        id: 4,
+        question : "What does the idiom 'to break the ice' mean?",
+        options : [
+            'To make a situation more tense',
+            'To start a conversation in a social setting',
+            'To conclude a discussion quickly'
+        ]
+    },
+    {
+        id: 5,
+        question : "Which word is a synonym for 'happy'?",
+        options : [
+            'Sad',
+            'Angry',
+            'Joyful',
+        ]
+    }
+];
+
+export const answers = [1, 2, 2, 1, 2];
